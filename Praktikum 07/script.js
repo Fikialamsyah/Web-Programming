58881ba358c1f797b06f37fda87c9cc7051d8952
fikialamsyah <fikialamsyah16@gmail.com>
2021-12-22 11:54:28 +0700

    add praktikum 07

---
+++ Praktikum 07/script.js
@@ -0,0 +1,26 @@
+console.log('Hello, Fika');
+
+// variable
+// 1. statis -> const
+const phi = 3.14;
+// phi = 3.15; error tidak bisa dikasih nilai baru
+console.log(phi);
+
+// 2. dynamis -> let
+let age = 20;
+age = 21;
+age = 22;
+age = 23;
+console.log(age);
+
+// cara penamaan variable
+let x = 'Fiki';
+let nama = 'Fiki';
+let nm = 'Fiki';
+// camel case
+let namaDepan = "Fiki";
+let namaBelakang = "alamsyah";
+
+// snake case
+let nama_depan = 'Fiki';
+let nama_belakang = 'Fiki alamsyah';
